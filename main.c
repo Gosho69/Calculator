599f5874ca57c8ff6348b8f24466b474508e5719
Merdzhak <alex.merdzhanov@gmail.com>
2025-05-27 19:52:07 +0300

    1st token function

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_INPUT 1024
+#define MAX_TOKENS 256
+
 const char *valid_operations[] = {
     "+", "-", "*", "/", "ln", "log", "pi", "e"};
 const int num_operations = 8;
@@ -15,4 +18,20 @@ int is_valid_operation(const char *token)
             return 1;
     }
     return 0;
+}
+
+int is_integer(const char *token)
+{
+    if (*token == '-' || *token == '+')
+        token++;
+    if (!*token)
+        return 0;
+
+    while (*token)
+    {
+        if (!isdigit(*token))
+            return 0;
+        token++;
+    }
+    return 1;
 }
