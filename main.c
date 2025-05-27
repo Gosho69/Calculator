0409777a844b5b99f197df2b2b0922e8be590869
Merdzhak <alex.merdzhanov@gmail.com>
2025-05-27 19:56:15 +0300

    Token input function

--- main.c
+++ main.c
@@ -35,3 +35,46 @@ int is_integer(const char *token)
     }
     return 1;
 }
+
+int tokenize_input(char tokens[][32], int *token_count)
+{
+    char input[MAX_INPUT];
+    printf("Enter postfix equation\n> ");
+
+    if (!fgets(input, sizeof(input), stdin))
+    {
+        fprintf(stderr, "There was an error with the input\n");
+        return 0;
+    }
+
+    input[strcspn(input, "\n")] = 0;
+
+    char *token = strtok(input, " \t");
+    *token_count = 0;
+
+    while (token != NULL)
+    {
+        if (*token_count >= MAX_TOKENS)
+        {
+            fprintf(stderr, "Too many tokens\n");
+            return 0;
+        }
+
+        if (!is_integer(token) && !is_valid_operation(token))
+        {
+            fprintf(stderr, "Invalid token: '%s'\n", token);
+            return 0;
+        }
+
+        strcpy(tokens[(*token_count)++], token);
+        token = strtok(NULL, " \t");
+    }
+
+    if (*token_count == 0)
+    {
+        fprintf(stderr, "The equation is empty\n");
+        return 0;
+    }
+
+    return 1;
+}
