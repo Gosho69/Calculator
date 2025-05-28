2b9c71df5447ed583a99f4473c6f29e65cbe9dd7
Gosho69 <gogchob08@gmail.com>
2025-05-28 20:02:06 +0300

    created main.h file

--- main.c
+++ main.c
@@ -3,13 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
-
-#define MAX_INPUT 1024
-#define MAX_TOKENS 256
-
-#define MY_PI 3.14159265358979323846
-#define MY_E 2.71828182845904523536
-// тея макрота са MY, за да не стане грещка със вече съществуващи макрота
+#include "main.h"
 
 const char *valid_operations[] = {
     "+", "-", "*", "/", "ln", "log", "pi", "e"};
@@ -90,7 +84,7 @@ double add(double a, double b)
 }
 
 double subtract(double a, double b)
-{ // второто се вади от първото
+{
     return a - b;
 }
 
@@ -100,7 +94,7 @@ double multiply(double a, double b)
 }
 
 double divide(double a, double b)
-{ // първото се дели на второто
+{
     if (b == 0)
     {
         printf("Error. Can't divide by zero!");
@@ -120,7 +114,7 @@ double e()
 }
 
 double ln(double a)
-{ // Натурален логаритъм
+{
     if (a <= 0)
     {
         printf("Error: ln(x), x must be > 0.\n");
@@ -130,7 +124,7 @@ double ln(double a)
 }
 
 double log_base(double a, double base)
-{ // Логаритъм при произволна основа
+{
     if (a <= 0 || base <= 0 || base == 1)
     {
         printf("Error: log_b(a), a > 0, base > 0 , base != 1.\n");
---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAX_INPUT 1024
+#define MAX_TOKENS 256
+
+#define MY_PI 3.14159265358979323846
+#define MY_E 2.71828182845904523536
+// тея макрота са MY, за да не стане грещка със вече съществуващи макрота
+
+int is_valid_operation(const char *token);
+int is_integer(const char *token);
+int tokenize_input(char tokens[][32], int *token_count);
+
+double add(double a, double b);
+double subtract(double a, double b); // второто се вади от първото
+double multiply(double a, double b);
+double divide(double a, double b); // първото се дели на второто
+double pi(void);
+double e(void);
+double ln(double a);                    // Натурален логаритъм
+double log_base(double a, double base); // Логаритъм при произволна основа
+
+#endif
